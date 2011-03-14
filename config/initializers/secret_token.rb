0faa64fc18cc908e3fb1852620dfa705f23b59f6
Daniel Valencia <danvalencia@gmail.com>
2011-03-13 20:37:18 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mundoyogacms::Application.config.secret_token = '0ca0e656707ca2cc6c8a3a23468dfa5354b9738e9576d26c5690344d76ecf6923bc2a513fbb68db285831fd19595e103ef2ee2dd28665f33bb9112f534510962'
